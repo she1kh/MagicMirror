73c5bf9aaad43cd1e4ce8ef6a72800d273af4472
Usman Sheikh <usman.sheikh@atea.se>
2018-08-16 13:57:44 +0200

    upload the key temp file

---
+++ assets/keys-temp.js
@@ -0,0 +1,15 @@
+//This file name should be keys.js
+module.exports = {
+    weahter: {
+        weatherKey:'key from openweather'    
+    },
+    mLabDB: {
+        username:'username',
+        password:'password',
+        ending:'port / name of your db'
+    },
+    outlook:{
+        OUTLOOK_CLIENT_ID:'the id',
+        OUTLOOK_CLIENT_SECRET:'the secret'
+    }
+}
